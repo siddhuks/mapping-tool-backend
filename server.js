b52e92d49b8e3b9c03add3a5f39a5607d8885353
Siddhartha K S <siddharthakshridhar@gmail.com>
2025-02-05 22:47:51 +0000

    changes in server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = require('./src/app');
 //     });
 // });
 
-const PORT = process.env.PORT || 8084;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
